47953e43571620a33ee2036426bb7692931c2b68
Lumi Kalt <lumi.mia.kalt@gmail.com>
2022-08-04 00:39:08 +0100

    Setup Readme

--- include/birds.hpp
+++ include/birds.hpp
@@ -1,136 +1,179 @@
+#pragma once
 #define fn auto
 
 namespace combinators {
 
+// λab.ab
+fn apply = [](fn a) {
+    return [=](auto&& b) {
+        return a(b);
+    }
+};
+
+
+// λabc.a(bc)
 fn bluebird = [](fn a, fn b) {
     return [=](auto&& c) {
         return a(b(c));
     };
 };
 
+// λabcd.a(bcd)
 fn blackbird = [](fn a, fn b) {
     return [=](auto&& c, auto&& d) {
         return a(b(c, d));
     };
 };
 
+// λabcde.a(bcde)
 fn bunting = [](fn a, fn b) {
     return [=](auto&& c, auto&& d, auto&& e) {
         return a(b(c, d, e));
     };
 };
 
+// λabcd.a(c(cd))
 fn becard = [](fn a, fn b, fn c) {
     return [=](auto&& d) {
         return a(b(c(d)));
     };
 };
 
+// λabc.acb
 fn cardinal = [](fn a) {
     return [=](auto&& b, auto&& c) {
         return a(c, b);
     };
 };
 
+// λabcd.ab(cd)
 fn dove = [](fn a, fn c) {
     return [=](auto&& b, auto&& d) {
         return a(b, c(d));
     };
 };
 
+// λabcde.abc(de)
 fn dickcissel = [](fn a, fn d) {
     return [=](auto&& b, auto&& c, auto&& e) {
         return a(b, c, d(e));
     };
 };
 
+// λabcde.a(bc)(de)
 fn dovekies = [](fn a, fn b, fn d) {
     return [=](auto&& c, auto&& e) {
         return a(b(c), d(e));
     };
 };
 
+// λabcde.ab(cde)
 fn eagle = [](fn a, fn c) {
     return [=](auto&& b, auto&& d, auto&& e) {
         return a(b, c(d, e));
     };
 };
 
+// λabcdefg.a(bcd)(efg)
 fn bald_eagle = [](fn a, fn d, fn e) {
     return [=](auto&& b, auto&& c, auto&& f, auto&& g) {
         return a(b(c, d), e(f, g));
     };
 };
 
+// λabc.cba
 fn finch = [](fn c) {
     return [=](auto&& a, auto&&b) {
         return c(b, a);
     };
 };
 
+// λabcd.ad(bc)
 fn goldfinch = [](fn a, fn b) {
     return [=](auto&& c, auto&& d) {
         return a(d, b(c));
     };
 };
 
+// λabc.abcb
 fn hummingbird = [](fn a) {
     return [=](auto&& b, auto&&c) {
         return a(b, c, b);
     };
 };
 
+// λa.a
 fn idiot = [](auto&& a) {
     return a;
 };
 
+// λabcd.ab(adc)
 fn jay = [](fn a) {
     return [=](auto&& b, auto&& c, auto&& d) {
         return a(b, a(d, c));
     };
 };
 
+// λab.a
 fn kestrel = [](auto&& a, auto&& b) {
     return a;
 };
 
+// λab.b
 fn kite = [](auto&& a, auto&& b) {
     return b;
 };
 
+// λab.a(bb)
 fn lark = [](fn a, fn b) {
-    return [=](auto&& x) {
+    return [=](auto&& x) { // :(
         return a(b(b(x)));
     };
 };
 
+// λa.aa
 fn mockingbird = [](fn a) {
-    return [=](auto&& x) {
+    return [=](auto&& x) { // :(
         return a(a(x));
     };
 };
 
+// λab.ab(ab)
 fn double_mockingbird = [](fn a) {
     return [=](auto&& b) {
         return a(b, a(b));
     };
 };
 
+// λab.b(ab)
 fn owl = [](fn a, fn b) {
-    return [=](auto&& x) {
+    return [=](auto&& x) { //:(
         return b(a(b(x)));
     };
 };
 
+/*
+// This isn't really possible to represent
+// in actual code, as far as I can tell.
+// λa.(aa)(aa)
 fn omega = []() {};
+*/
 
+// λabc.b(a, c)
 fn phoenix = [](fn a, fn b, fn c) {
     return [=](auto&& x) {
         return b(a(x), c(x));
     };
 };
+// Could technically think of a and c as
+// values themselves... alas.
 
-fn psi = []() {};
+// λabcd.a(bc)(bd)
+fn psi = [](fn a, fn b) {
+    return [=](auto&& c, auto&& d) {
+        return a(b(c), b(d));
+    }
+};
 
 fn queer = []() {};
 
@@ -160,5 +203,5 @@ fn _m2  = double_mockingbird;
 fn _o   = owl;
 fn _phi = phoenix;
 fn _psi = psi;
-
+//fn _om  = omega;
 } // namespace combinators
